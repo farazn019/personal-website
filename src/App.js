4ff98ad737da8fcd745af5012278050f335989a3
Faraz Naseem <naseemf@uwindsor.ca>
2023-10-12 18:58:41 -0400

    fixed margins below resume and changed resume

--- src/App.js
+++ src/App.js
@@ -4,31 +4,13 @@ import { AboutMe } from './AboutMe';
 import './App.css';
 import { WorkExperience } from './WorkExperience';
 
-
-import Faraz_Naseem_Resume from './pdf_files/Faraz_Naseem_Resume.pdf';
-
 import { Document, Page, pdfjs} from 'react-pdf';
-import { useState } from 'react';
 
-/*pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
-  import.meta.url,
-).toString();*/
+import resume from './pdf_files/Faraz_Naseem_Resume.pdf';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function App() {
-
-
-  /*const [numPages, setNumPages] = useState(1);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess (numPages) {
-    setNumPages(numPages);
-  }*/
-
-  const resume = './Faraz_Naseem_Resume.pdf';
-
 
   return (
     <div className="app">
@@ -47,13 +29,6 @@ function App() {
 
       <div className="embed-responsive" id="resume" style = {{height: '100vh'}}>
 
-        {/*<PDFViewer>
-          <PDFDocument></PDFDocument>
-        </PDFViewer>*/}
-
-        {/*<Document options={{workerSrc: './pdf_files/Faraz_Naseem_Resume.pdf'}}>
-          <Page></Page>
-        </Document>*/}
         <div style={{width: 600}} className="resume-document">
           <Document file={resume}>
             <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} customTextRenderer={false}></Page> 
@@ -63,15 +38,9 @@ function App() {
 
 
         </div>
+        
       </div>
 
-      {/*<div className="side-projects" id="side-projects">
-
-      </div>
-
-      <div className="contact-me" id="contact-me">
-
-      </div>*/}
     </div>
   );
 }
